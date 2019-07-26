d7249f0528ceba0328c6a41bb092a842df67d91e
mnguyen <mnguyen@dynamicsignal.com>
2019-07-26 12:46:59 -0700

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by My Nguyen on 7/26/19.
+//  Copyright © 2019 My Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
