7ab82baf22274355925e806850836b07222d8f21
mnguyen <mnguyen@dynamicsignal.com>
2019-08-01 18:08:51 -0700

    First commit

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+
+class Concentration  {
+    var cards = [Card]()
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    init(numberOfPairsOfCards: Int) {
+        for _ in 1...numberOfPairsOfCards {
+            let card = Card()
+            // assignment makes a copy of card
+            // let matchingCard = card
+            // this also makes a copy
+            // cards.append(card)
+            // cards.append(card)
+            // another way of adding 2 cards to the cards array
+            cards += [card, card]
+        }
+        // TODO: shuffle the cards
+    }
+
+    func chooseCard(at index: Int) {
+        /* if cards[index].isFaceUp {
+            cards[index].isFaceUp = false
+        } else {
+            cards[index].isFaceUp = true
+        } */
+        cards[index].isFaceUp = !cards[index].isFaceUp
+        
+        if !cards[index].isMatched {
+            if let matchInex = indexOfOneAndOnlyFaceUpCard, matchInex != index {
+                // check if cards match
+                if cards[matchInex].identifier == cards[index].identifier {
+                    cards[matchInex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                // either no cards or 2 cards are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+}
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,11 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count+1) / 2)
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            // flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+        } else {
+            print("chosen card was not in cardButtons")
+        }
     }
 
+    /* func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    } */
+    
+    func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: UIControl.State.normal)
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            } else {
+                button.setTitle("", for: UIControl.State.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+            }
+        }
+    }
 
+    // bat, surprised, scared, devil, pumpkin, ghost, lollipop, lollipop, apple
+    var emojiChoices = ["🦇", "😱", "🙀", "😈", "🎃", "👻", "🍭", "🍬", "🍎"]
+    // var emoji = Dictionary<Int, String>()
+    var emoji = [Int:String]()
+
+    func emoji(for card: Card) -> String {
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+        }
+        /* if emoji[card.identifier] != nil {
+            return emoji[card.identifier]!
+        } else {
+            return "?"
+        } */
+        return emoji[card.identifier] ?? "?"
+    }
 }
-
