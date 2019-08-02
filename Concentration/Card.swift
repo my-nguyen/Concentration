4da5b93e8608ae1d9d4846c965d8d0f903ec562a
mnguyen <mnguyen@dynamicsignal.com>
2019-08-01 18:08:06 -0700

    First commit

---
+++ Concentration/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by My Nguyen on 7/31/19.
+//  Copyright © 2019 My Nguyen. All rights reserved.
+//
+
+import Foundation
---
+++ Concentration/Concentration.swift
@@ -0,0 +1,9 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by My Nguyen on 7/31/19.
+//  Copyright © 2019 My Nguyen. All rights reserved.
+//
+
+import Foundation
